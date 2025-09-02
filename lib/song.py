a257042c0715815117ad69a3239bc70d7b9925e6
keithmulwa <keithfax2@gmsil.com>
2025-09-02 10:12:17 +0300

    first

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,23 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update class-level stats
+        Song.count += 1
+
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
